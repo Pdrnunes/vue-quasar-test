24e21a81b66d4e6f00c504d7f4a8768b3685da93
Pdrnunes <bttnns@gmail.com>
2021-11-10 20:15:50 -0300

    adding hardcoded and exporting properties to the page.

--- vue-quasar-test/src/pages/Index.vue
+++ vue-quasar-test/src/pages/Index.vue
@@ -1,6 +1,9 @@
 <template>
-  <q-page>
+  <q-page padding>
     <!-- all quasar pages root element -->
+    <!-- <h5>introducing titles in my app</h5> lesson 2: manual hardcoded message -->
+    <h5>{{message}}</h5>
+    <!-- lesson 2: exporting the property message to page -->
   </q-page>
 </template>
 
@@ -9,9 +12,15 @@
 
 <script>
 // where all data and methods should go
+// lesson 2: creating a data function which returns an object
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  
+  data() {
+    return {
+      // here is where all properties will go:
+      message: 'Introducing titles in my app' //lesson 2 more adaptability if a further method changes the content of this property.
+    }
+  }
 })
 </script>

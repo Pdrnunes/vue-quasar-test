8b09475101678d4abaf8774c17dfac73528e034b
Pdrnunes <bttnns@gmail.com>
2021-11-10 19:56:57 -0300

    first lesson: pointing the main folders and the first view of the index page

---
+++ vue-quasar-test/src/pages/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page>
+    <!-- all quasar pages root element -->
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+// where all data and methods should go
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  
+})
+</script>

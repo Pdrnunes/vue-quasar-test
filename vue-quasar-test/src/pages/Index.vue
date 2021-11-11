24f8eedff45f8010b7400f75bb0c368cf1a23a86
Pdrnunes <bttnns@gmail.com>
2021-11-10 21:07:19 -0300

    hardcoding and  component method for editing an input field

--- vue-quasar-test/src/pages/Index.vue
+++ vue-quasar-test/src/pages/Index.vue
@@ -2,6 +2,11 @@
   <q-page padding>
     <!-- lesson 3 - editing a property in realtime with v-model directive: -->
     <input v-model="message"/>
+    <!-- lesson 4 - attaching event to a button: -->
+    <!-- lesson 4 obs.: @click supports javascript coding event -->
+    <!-- <button @click="message = ''">Clear</button> -->
+    <!-- lesson 4 - clearing message implementing a method: -->
+    <button @click="clearMessage"></button>
     <!-- all quasar pages root element -->
     <!-- <h5>introducing titles in my app</h5> lesson 2: manual hardcoded message -->
     <h5>{{message}}</h5>
@@ -22,6 +27,11 @@ export default defineComponent({
     return {
       // here is where all properties will go:
       message: 'Introducing titles in my app' //lesson 2 more adaptability if a further method changes the content of this property.
+    }
+  },
+  methods: {
+    clearMessage() { //the method for clearing the message editor
+      this.message = ''
     }
   }
 })
